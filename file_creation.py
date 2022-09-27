c908dac3e1b32420d63aff65337c6d2ec4df6c6b
joel-n <jof.nilsson@gmail.com>
2022-09-27 15:37:04 +0200

    Relate user interact. to infl. interact. in time

--- file_creation.py
+++ file_creation.py
@@ -1,6 +1,7 @@
 import os
-from file_manip import file_generator, append_objs_to_file
-from analysis_tools import get_file_paths
+from numpy import array as nparr
+from file_manip import file_generator, append_objs_to_file, read_file, write_text
+from analysis_tools import get_file_paths, compute_relative_time
 
 
 def make_infl_id_file(conv_id: str, threshold: float, threshold_percentile: float) -> None:
@@ -13,7 +14,6 @@ def make_infl_id_file(conv_id: str, threshold: float, threshold_percentile: floa
         - threshold: threshold on followers to count as an influencer
         - threshold_percentile: Percentile (divided by 100) of local/global follower
         distribution used to compute the threshold. Will be part of the file name.
-    
     """
     threshold_percentile = str(threshold_percentile).replace('.', '')
     infl_id_path = f'infl_ids/{conv_id}_{threshold_percentile}.txt'
@@ -35,4 +35,79 @@ def make_infl_id_file(conv_id: str, threshold: float, threshold_percentile: floa
     
     append_objs_to_file(infl_id_path, [attr[0] for attr in infl_attr])
     append_objs_to_file(infl_nflw_path, [attr[1] for attr in infl_attr])
+    return
+
+
+def make_infl_flwr_matching(conv_ids: list[str]) -> None:
+    """Makes files containing interactions of influencers and influencer followers
+    for a set of conversations. Stored in the folder matched_infl. The output is
+    a .csv-file with influencer ID (implying that the user follows this user), user
+    ID, interaction time, and interaction type as columns. Interactions from influencers
+    are also added, where the user ID is simply the influencer user ID.
+    Note that all reply, retweet and quote files of the conversation must exist,
+    and there must be a list of follower IDs for each influencer in the conversation.
+
+    Args:
+        - conv_ids: a list of conversation IDs
+    """
+    already_matched, missing_data = 0, 0
+
+    for conv_id in conv_ids:
+    
+        s_path = f'matched_infl/{conv_id}_user_infl_interactions.txt'
+
+        if os.path.isfile(s_path):
+            already_matched += 1
+            continue
+            
+        # Read influencer files and see where the influencers interact with the conversation.
+        infl_ids = read_file(f'infl_ids/{conv_id}_095.txt')
+        existence = nparr([os.path.isfile(f'infl_follower_ids/{i_id}_followers.txt') for i_id in infl_ids])
+        
+        if not existence.all():
+            missing_data += 1
+            continue
+        
+        root = read_file(f'root_tweets/{conv_id}_root.jsonl')[0]
+        root_time = root['created_at']
+        interaction = {}
+
+        # Read interaction files of time stamps and the IDs of users interacting
+        file_paths = [
+            f'sampled_conversations/{conv_id}_conversation-tweets.jsonl',
+            f'retweets/{conv_id}.jsonl',
+            f'quotes/{conv_id}_quotes.jsonl'
+        ]
+
+        for path in file_paths:
+            interaction_type = path[0]
+            for tw in file_generator(path):
+                time, ID,  = tw['created_at'], tw['author_id']
+                t = compute_relative_time(root_time, time)
+                
+                if not ID in interaction:
+                    interaction[ID] = []
+                interaction[ID].append((t, interaction_type))
+        
+        # Read list of the followers of the influencers
+        # Store matching infl.-follower IDs, time difference and interaction types.
+        for i_id in infl_ids:
+            infl_follower_file, infl_id = f'infl_follower_ids/{i_id}_followers.txt', str(i_id)
+            
+            if infl_id in interaction:
+                infl_interactions = interaction[infl_id]
+                for ii in infl_interactions:
+                    dat = '{},{},{},{}'.format(infl_id, infl_id, ii[0], ii[1])
+                    write_text(s_path, dat)
+
+            for u_id in file_generator(infl_follower_file):
+                user_id = str(u_id)
+                
+                if user_id in interaction:
+                    user_interactions = interaction[user_id]
+                    for ui in user_interactions:
+                        dat = '{},{},{},{}'.format(infl_id, user_id, ui[0], ui[1])
+                        write_text(s_path, dat)
+    
+    print(f'Out of {len(conv_ids)} conversations, {already_matched} were already processed, and influencer follower IDs were missing for {missing_data}.')
     return
